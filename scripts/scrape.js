451f6fc34e8fba0ac4720136dbfae2b9e48f0f80
JTerry <jessica.terry79@yahoo.com>
2019-01-19 11:46:59 -0600

    correcting an error

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,32 +1,61 @@
-// Require scraping dependency tools
-var request = require("request");
+// Require axios and cheerio dependency to make our scrape
+var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Scrape the articles from the New YorK Times
-var scrape = function(callback) {
+// Function to scrape the New York Times website
+var scrape = function() 
+{
+    // Scrape the New York Times website
+    return axios.get("http://www.nytimes.com").then(function(res){
+        var $ = cheerio.load(res.data);
+        
+        // Log our scraping
+        console.log("scraping");
+        
+        // Articles array to save our article information
+        var articles = [];
 
-  var articlesArr = [];
+   
+    $("div.css-1100km").each(function(i, element) 
+    {
+        // Grab the heading of the article
+        var head = $(this)
+            .find("h2")
+            .text()
+            .trim();
 
-  request("http://www.nytimes.com/", function(error, response, html) {
+        // Grab the URL of the article
+        var url = $(this)
+            .find("a")
+            .attr("href");
 
-      var $ = cheerio.load(html);
+        // Grab the Summary of the article
+        var sum = $(this)
+            .find("p")
+            .text()
+            .trim();
 
+        // So long as our headline and sum and url aren't empty or undefined, do the following
+        if (head && sum && url) 
+        {
+            // Remove extra characters to establish a neat look
+            var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+            var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
 
-      $("h2.story-heading").each(function(i, element) {
+        // Push the data to the articles array
+        var dataToAdd = 
+        {
+            headline: headNeat,
+            summary: sumNeat,
+            url: "https://www.nytimes.com" + url
+        };
 
-          var result = {};
-
-          // Add the text and href of every link, and save them as properties of the result object
-          result.title = $(this).children("a").text();
-          result.link = $(this).children("a").attr("href");
-
-          if (result.title !== "" && result.link !== "") {
-              articlesArr.push(result);
-          }
-      });
-      callback(articlesArr);
+        articles.push(dataToAdd);
+      }
+    });
+    // Return the articles found to the user
+    return articles;
   });
-
 };
 
 // Export the function for other files to use
