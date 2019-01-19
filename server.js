db742376cbb17a2fa1831f020e05a9d42b9e2607
JTerry <jessica.terry79@yahoo.com>
2019-01-19 08:10:56 -0600

    updating an error in mongo

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ var express = require("express");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
 
-// Requiring our data models
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-
-// Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
+// Set port
+var Port = process.env.PORT || 3000;
 
 // Initialize Express application
 var app = express();
+
 
 // Parse request body as JSON configuration
 app.use(express.urlencoded({ extended: true }));
@@ -20,33 +17,22 @@ app.use(express.json());
 // Make public a static directory folder
 app.use(express.static('public'));
 
+// Set engine and default for handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 
 // Database configuration with mongoose
-var databaseUri = "mongodb://localhost/mongoosearticles";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-if (process.env.MONGODB_URI) {
+if (process.env.MONGODB_URI) 
+{
   mongoose.connect(process.env.MONGODB_URI);
 } 
 else 
 {
-  mongoose.connect(databaseUri);
+  mongoose.connect(MONGODB_URI);
 }
-
-// Variable assigned to the mongoose connection
-var db = mongoose.connection;
-
-db.on("error", function(error) 
-{
-  console.log("Mongoose Database Connection Error: ", error);
-});
-
-db.once("open", function() {
-  console.log("Mongoose connection wasmsuccessful.");
-});
-
-// Set engine and default for handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var router = express.Router();
@@ -57,10 +43,7 @@ require("./config/routes")(router);
 // Have every request go through router middlewar
 app.use(router);
 
-// Set port
-var port = process.env.PORT || 3001;
-
 // setup listener for the appliation
-app.listen(port, function() {
-  console.log("app running on port " + port);
+app.listen(Port, function() {
+  console.log("Listening app is running on port " + Port);
 });
